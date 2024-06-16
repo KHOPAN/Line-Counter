49f6dc6067fd33607b04e8b3b1f758d540e7499f
KHOPAN <puthipnakchan@gmail.com>
2024-06-16 14:15:02 +0700

    Implement recursive search

--- line-counter/linecounter.c
+++ line-counter/linecounter.c
@@ -1,14 +1,108 @@
 ﻿#include <stdio.h>
 #include <khopanwin32.h>
 #include <khopanstring.h>
+#include <ShlObj_core.h>
 #include "linecounter.h"
 
+static BOOL recursiveLineCount(const LPCWSTR fileExtension, BOOL recursiveSearch, BOOL separateFiles, const LPCWSTR searchPath, size_t* lines);
+
 int countLine(const LPCWSTR fileExtension, BOOL recursiveSearch, BOOL separateFiles, const LPCWSTR searchPath) {
-	LPWSTR search = KHFormatMessageW(L"%s\\*", searchPath);
+	DWORD pathFolderCurrentDirectoryLength = GetCurrentDirectoryW(0, NULL);
+
+	if(!pathFolderCurrentDirectoryLength) {
+		KHWin32ConsoleErrorW(GetLastError(), L"GetCurrentDirectoryW");
+		return 1;
+	}
+
+	LPWSTR pathFolderCurrentDirectory = LocalAlloc(LMEM_FIXED, pathFolderCurrentDirectoryLength * sizeof(WCHAR));
+
+	if(!pathFolderCurrentDirectory) {
+		KHWin32ConsoleErrorW(GetLastError(), L"LocalAlloc");
+		return 1;
+	}
+
+	if(!GetCurrentDirectoryW(pathFolderCurrentDirectoryLength, pathFolderCurrentDirectory)) {
+		KHWin32ConsoleErrorW(GetLastError(), L"GetCurrentDirectoryW");
+		return 1;
+	}
+
+	LPWSTR pathFolderRawSearch = KHFormatMessageW(L"%ws\\%ws", pathFolderCurrentDirectory, searchPath);
+	LocalFree(pathFolderCurrentDirectory);
+
+	if(!pathFolderRawSearch) {
+		KHWin32ConsoleErrorW(ERROR_FUNCTION_FAILED, L"KHFormatMessageW");
+		return 1;
+	}
+
+	WCHAR path[MAX_PATH];
+	memset(path, 0, MAX_PATH);
+	size_t rawSearchLength = wcslen(pathFolderRawSearch);
+
+	for(size_t i = 0; i < min(rawSearchLength, MAX_PATH); i++) {
+		path[i] = pathFolderRawSearch[i];
+	}
+
+	LocalFree(pathFolderRawSearch);
+
+	if(!PathResolve(path, NULL, 0)) {
+		KHWin32ConsoleErrorW(GetLastError(), L"PathResolve");
+		return 1;
+	}
+
+	size_t lines = 0;
+
+	if(!recursiveLineCount(fileExtension, recursiveSearch, separateFiles, path, &lines)) {
+		KHWin32ConsoleErrorW(GetLastError(), L"recursiveLineCount");
+		return 1;
+	}
+
+	printf("Lines: %llu\n", lines);
+	return 0;
+}
+
+static BOOL checkFileExtension(const LPCWSTR filePath, const LPCWSTR extension) {
+	if(!extension) {
+		return TRUE;
+	}
+
+	size_t extensionLength = wcslen(extension);
+
+	if(!extensionLength) {
+		return TRUE;
+	}
+
+	size_t fileLength = wcslen(filePath);
+
+	if(fileLength <= extensionLength) {
+		return FALSE;
+	}
+
+	size_t offset = fileLength - extensionLength;
+
+	if(filePath[offset - 1] != L'.') {
+		return FALSE;
+	}
+
+	for(size_t i = 0; i < extensionLength; i++) {
+		if(towlower(filePath[i + offset]) != towlower(extension[i])) {
+			return FALSE;
+		}
+	}
+
+	return TRUE;
+}
+
+static BOOL countFileLines(const LPCWSTR fileName, size_t* lines) {
+	(*lines)++;
+	return TRUE;
+}
+
+static BOOL recursiveLineCount(const LPCWSTR fileExtension, BOOL recursiveSearch, BOOL separateFiles, const LPCWSTR searchPath, size_t* lines) {
+	LPWSTR search = KHFormatMessageW(L"%ws\\*", searchPath);
 
 	if(!search) {
 		KHWin32ConsoleErrorW(ERROR_FUNCTION_FAILED, L"KHFormatMessageW");
-		return 1;
+		return FALSE;
 	}
 
 	WIN32_FIND_DATAW data;
@@ -17,23 +111,58 @@ int countLine(const LPCWSTR fileExtension, BOOL recursiveSearch, BOOL separateFi
 
 	if(searchHandle == INVALID_HANDLE_VALUE) {
 		KHWin32ConsoleErrorW(GetLastError(), L"FindFirstFileW");
-		return 1;
+		return FALSE;
 	}
 
 	BOOL noError = FALSE;
+	BOOL returnValue = FALSE;
 
 	do {
-		wprintf(L"%d %ws\n", data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY, data.cFileName);
-	} while(noError = FindNextFileW(searchHandle, &data));
+		if(!wcscmp(data.cFileName, L".") || !wcscmp(data.cFileName, L"..")) {
+			continue;
+		}
 
-	int returnValue = 1;
+		LPWSTR pathName = KHFormatMessageW(L"%ws\\%ws", searchPath, data.cFileName);
+
+		if(!pathName) {
+			KHWin32ConsoleErrorW(ERROR_FUNCTION_FAILED, L"KHFormatMessageW");
+			goto closeSearchHandle;
+		}
+
+		if(!(data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+			if(!checkFileExtension(data.cFileName, fileExtension)) {
+				LocalFree(pathName);
+				continue;
+			}
+
+			BOOL value = countFileLines(pathName, lines);
+
+			if(!value) {
+				returnValue = value;
+				goto closeSearchHandle;
+			}
+
+			LocalFree(pathName);
+			continue;
+		}
+
+		if(recursiveSearch) {
+			BOOL value = recursiveLineCount(fileExtension, recursiveSearch, separateFiles, pathName, lines);
+			LocalFree(pathName);
+
+			if(!value) {
+				returnValue = value;
+				goto closeSearchHandle;
+			}
+		}
+	} while(noError = FindNextFileW(searchHandle, &data));
 
 	if(!noError && GetLastError() != ERROR_NO_MORE_FILES) {
 		KHWin32ConsoleErrorW(GetLastError(), L"FindNextFileW");
 		goto closeSearchHandle;
 	}
 
-	returnValue = 0;
+	returnValue = TRUE;
 closeSearchHandle:
 	FindClose(searchHandle);
 	return returnValue;
